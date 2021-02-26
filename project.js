5585020a67ba8098add1db6dfaf46acc74d6ff88
Csikós András <csikosandras3@gmail.com>
2021-02-26 16:02:32 +0100

    valami

---
+++ project.js
@@ -0,0 +1,8 @@
+const apple = 30;
+const banana = apple*2;
+const lemon = apple *3;
+console.log(apple,'dkg');
+console.log(banana,'dkg');
+console.log(lemon,'dkg');
+let ossz = apple+banana+lemon;
+console.log(ossz/100,'kg');
